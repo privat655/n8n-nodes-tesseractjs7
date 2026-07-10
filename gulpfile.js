ef6cbf05461258185e12922a543710103bfc43a9
privat655 <todor.dimitrov@outlook.com>
2026-07-10 15:36:27 +0200

    Harden dependency setup and icon build

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,28 @@
 const path = require('path');
-const { task, src, dest } = require('gulp');
+const { existsSync } = require('fs');
+const { task, src, dest, series } = require('gulp');
 
 task('build:icons', copyIcons);
 
-function copyIcons() {
+function copyNodeIcons() {
 	const nodeSource = path.resolve('nodes', '**', '*.{png,svg}');
 	const nodeDestination = path.resolve('dist', 'nodes');
 
-	src(nodeSource).pipe(dest(nodeDestination));
+	return src(nodeSource, { allowEmpty: true }).pipe(dest(nodeDestination));
+}
+
+function copyCredentialIcons(done) {
+	if (!existsSync(path.resolve('credentials'))) {
+		done();
+		return;
+	}
 
 	const credSource = path.resolve('credentials', '**', '*.{png,svg}');
 	const credDestination = path.resolve('dist', 'credentials');
 
-	return src(credSource).pipe(dest(credDestination));
+	return src(credSource, { allowEmpty: true }).pipe(dest(credDestination));
+}
+
+function copyIcons(done) {
+	return series(copyNodeIcons, copyCredentialIcons)(done);
 }
